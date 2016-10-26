f82c89926d44e52e7b578ee2e05d97e9a6f290d4
Gene Der Su <e870252314@yahoo.com.tw>
2016-10-26 14:55:03 -0700

    feat: complete question 4
    Completed question 4 with test cases and explanations.

--- Solutions.py
+++ Solutions.py
@@ -146,8 +146,99 @@ def question3(G):
 
 
 ############# For Question4 #############
-def question4(T, r, n1, n2):
-    pass
+class Tree_Node(object):
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+def BST_seatch(r, n):
+    # search if n is in the tree
+    current_node = r
+    while current_node.left != None or current_node.right != None:
+
+        # go left if current node is greater than n
+        if current_node.data > n:
+            # make sure left node exist
+            if current_node.left != None:
+                current_node = current_node.left
+            else:
+                return False
+
+        # go right if current node is less than n
+        elif current_node.data < n:
+            # make sure right node exist
+            if current_node.right != None:
+                current_node = current_node.right
+            else:
+                return False
+
+        # current node is n, return True
+        else:
+            return True
+        
+    # check if n is in the lowest level
+    if current_node.data == n:
+        return True
+    else:
+        return False
+
+def question4(r, n1, n2):
+    # make sure r is a tree node
+    if type(r) != Tree_Node:
+        return "Error: r not Tree_Node!"
+
+    # make sure n1 and n2 are integers
+    if type(n1) != int:
+        return "Error: n1 not integer!"
+    if type(n2) != int:
+        return "Error: n2 not integer!"
+
+    # make sure n1 and n2 in the tree
+    if not BST_seatch(r, n1):
+        return "Error: n1 not in the tree!"
+    if not BST_seatch(r, n2):
+        return "Error: n2 not in the tree!"
+    
+    current_node = r
+    while current_node.left != None or current_node.right != None:
+        
+        # if both vales are less than the current node, we go to the left
+        if current_node.data > n1 and current_node.data > n2:
+            current_node = current_node.left
+
+        # if both vales are greater than the current node, we go to the right
+        elif current_node.data < n1 and current_node.data < n2:
+            current_node = current_node.right
+
+        # we find the answer, return it
+        else:
+            return current_node.data
+    
+    # n1 equal to n2 at the lowest level
+    return current_node.data
+
+def test4():
+    n1, n3, n5, n7 = Tree_Node(1), Tree_Node(3), Tree_Node(5), Tree_Node(7)
+    n9, n11, n13, n15 = Tree_Node(9), Tree_Node(11), Tree_Node(13), Tree_Node(15)
+    n2, n6, n10, n14 = Tree_Node(2), Tree_Node(6), Tree_Node(10), Tree_Node(14)
+    n2.left, n2.right = n1, n3
+    n6.left, n6.right = n5, n7
+    n10.left, n10.right = n9, n11
+    n14.left, n14.right = n13, n15
+    n4, n12 = Tree_Node(4), Tree_Node(12)
+    n4.left, n4.right = n2, n6
+    n12.left, n12.right = n10, n14
+    r = Tree_Node(8)
+    r.left, r.right = n4, n12
+    
+    print "\nTesting 4"
+    print "Edge case (r not Tree_Node):", "Pass" if "Error: r not Tree_Node!" == question4(123, 111, 111) else "Fail"
+    print "Edge case (n1 not in the tree):", "Pass" if "Error: n1 not in the tree!" == question4(r, -1, 5) else "Fail"
+    print "Case (n1 = 8 and n2 = 1):", "Pass" if 8 == question4(r, 8, 1) else "Fail" 
+    print "Case (n1 = 1 and n2 = 3):", "Pass" if 2 == question4(r, 1, 3) else "Fail"
+    print "Case (n1 = 9 and n2 = 15):", "Pass" if 12 == question4(r, 9, 15) else "Fail"
+    print "Case (n1 = 1 and n2 = 11):", "Pass" if 8 == question4(r, 1, 11) else "Fail"
 
 
 ############# For Question5 #############
@@ -225,5 +316,5 @@ def test5():
 #test1()
 #test2()
 #test3()
-#test4()
-test5()
+test4()
+#test5()
